b4c8713c6e7b103e4075a4a49af7290792313f2e
Sebastien <sebastien.andre.seb@gmail.com>
2017-11-08 21:51:26 +0100

    first commit

---
+++ src/main/java/com/mobylis/fr/mobylis/MobylisApplication.java
@@ -0,0 +1,12 @@
+package com.mobylis.fr.mobylis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobylisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobylisApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mobylis/fr/mobylis/MobylisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mobylis.fr.mobylis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MobylisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
